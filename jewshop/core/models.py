7c7398eabe9ccdc3879d5609e7ebec719ce4132b
Daniil-Solo <daniil.solo1723@gmail.com>
2022-02-16 22:51:17 +0500

    add models

--- jewshop/core/models.py
+++ jewshop/core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Jewelry(models.Model):
+    title = models.CharField(max_length=200, verbose_name="заголовок")
+    slug = models.SlugField(unique=True, max_length=100, verbose_name="слаг")
+    description = models.TextField(null=True, blank=True, verbose_name="описание")
+    is_published = models.BooleanField(default=False, verbose_name="опубликовано")
+
+    jew_cat = models.ForeignKey(
+        to="Category",
+        on_delete=models.SET_DEFAULT,
+        default="Нет категории",
+        verbose_name="категория украшения"
+    )
+    potpourri_cat = models.ManyToManyField(
+        to="Potpourri",
+        verbose_name="сухоцвет"
+    )
+    material_cat = models.ManyToManyField(
+        to="Material",
+        verbose_name="материал"
+    )
+
+    price = models.FloatField(verbose_name="цена")
+    weight = models.FloatField(null=True, blank=True, verbose_name="вес")
+    length = models.FloatField(null=True, blank=True, verbose_name="длина")
+    width = models.FloatField(null=True, blank=True, verbose_name="ширина")
+
+    date_create = models.DateField(auto_now_add=True, verbose_name="дата создания")
+    date_edit = models.DateField(auto_now=True, verbose_name="дата редактирования")
+
+
+class Gallery(models.Model):
+    image = models.ImageField(upload_to='gallery')
+    jewelry = models.ForeignKey(to="Jewelry", on_delete=models.CASCADE, related_name='images')
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name="название")
+
+
+class Potpourri(models.Model):
+    title = models.CharField(max_length=50, verbose_name="название")
+    slug = models.SlugField(unique=True, max_length=50, verbose_name="слаг")
+    image = models.ImageField(upload_to='images/%Y/%m/%d/', max_length=100, verbose_name="изображения")
+    description = models.TextField(null=True, blank=True, verbose_name="описание")
+
+
+class Material(models.Model):
+    title = models.CharField(max_length=50, verbose_name="название")
