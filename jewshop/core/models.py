4e4ba26961e973c6ddb66aa59846449e127fcbc3
Daniil-Solo <daniil.solo1723@gmail.com>
2022-02-19 00:22:08 +0500

    rename models and add fields

--- jewshop/core/models.py
+++ jewshop/core/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Jewelry(models.Model):
     title = models.CharField(max_length=200, verbose_name="заголовок")
     slug = models.SlugField(unique=True, max_length=100, verbose_name="слаг")
+
     description = models.TextField(null=True, blank=True, verbose_name="описание")
-    is_published = models.BooleanField(default=False, verbose_name="опубликовано")
+    extra = models.TextField(null=True, blank=True, verbose_name="дополнительно")
+    is_in_stock = models.BooleanField(default=False, verbose_name="в наличии")
 
     jew_cat = models.ForeignKey(
         to="Category",
@@ -13,15 +15,14 @@ class Jewelry(models.Model):
         default="Нет категории",
         verbose_name="категория украшения"
     )
-    potpourri_cat = models.ManyToManyField(
-        to="Potpourri",
-        verbose_name="сухоцвет"
-    )
     material_cat = models.ManyToManyField(
         to="Material",
         verbose_name="материал"
     )
-
+    metal_cat = models.ManyToManyField(
+        to="Metal",
+        verbose_name="фурнитура"
+    )
     price = models.FloatField(verbose_name="цена")
     weight = models.FloatField(null=True, blank=True, verbose_name="вес")
     length = models.FloatField(null=True, blank=True, verbose_name="длина")
@@ -32,7 +33,7 @@ class Jewelry(models.Model):
 
 
 class Gallery(models.Model):
-    image = models.ImageField(upload_to='gallery')
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/')
     jewelry = models.ForeignKey(to="Jewelry", on_delete=models.CASCADE, related_name='images')
 
 
@@ -40,12 +41,12 @@ class Category(models.Model):
     title = models.CharField(max_length=50, unique=True, verbose_name="название")
 
 
-class Potpourri(models.Model):
+class Material(models.Model):
     title = models.CharField(max_length=50, verbose_name="название")
     slug = models.SlugField(unique=True, max_length=50, verbose_name="слаг")
-    image = models.ImageField(upload_to='images/%Y/%m/%d/', max_length=100, verbose_name="изображения")
+    image = models.ImageField(upload_to='materials/', max_length=100, verbose_name="изображение")
     description = models.TextField(null=True, blank=True, verbose_name="описание")
 
 
-class Material(models.Model):
+class Metal(models.Model):
     title = models.CharField(max_length=50, verbose_name="название")
