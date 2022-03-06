ff01d8700c254593e9e1eb811fb9c7e9a1a37294
Daniil-Solo <daniil.solo1723@gmail.com>
2022-03-06 18:02:09 +0500

    update links, optimize template with mixin

--- jewshop/core/utils.py
+++ jewshop/core/utils.py
@@ -1,18 +1,29 @@
+from django.urls import reverse
+
+
 class MenuMixin:
     @staticmethod
     def get_menu_context_data(**kwargs):
         context = kwargs
-        context["for_buyers"] = [
-            dict(title="Доставка", url="catalog"),
-            dict(title="Оплата", url="catalog"),
-            dict(title="Уход", url="catalog"),
-        ]
-        context["info"] = [
-            dict(title="О нас", url="catalog"),
-        ]
-        context["contacts"] = [
-            dict(title="Вконтакте", url="https://vk.com/fragilemystery"),
-            dict(title="Instagram", url="https://www.instagram.com/fragilemystery/"),
-            dict(title="Telegram", url="https://t.me/fragilemystery"),
+        context["footer"] = [
+            dict(title="Покупателям",
+                 sub_items=[
+                     dict(title="Доставка", url=reverse("catalog")),
+                     dict(title="Оплата", url=reverse("catalog")),
+                     dict(title="Уход", url=reverse("catalog")),
+                    ]
+                 ),
+            dict(title="Информация",
+                 sub_items=[
+                     dict(title="О нас", url=reverse("catalog")),
+                    ]
+                 ),
+            dict(title="Контакты",
+                 sub_items=[
+                     dict(title="Вконтакте", url="https://vk.com/fragilemystery"),
+                     dict(title="Instagram", url="https://www.instagram.com/fragilemystery/"),
+                     dict(title="Telegram", url="https://t.me/fragilemystery"),
+                    ]
+                 ),
         ]
         return context
