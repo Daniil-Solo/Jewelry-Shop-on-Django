14bba3af85a02fc026b8c515ed495a93b1d841c9
Daniil-Solo <daniil.solo1723@gmail.com>
2022-04-07 23:09:26 +0500

    add favicon

--- jewshop/core/urls.py
+++ jewshop/core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from .views import *
 
 
 urlpatterns = [
     path('catalog/', JewelryCatalog.as_view(), name='catalog'),
     path('jewelries/<slug:jew_slug>/', JewelryView.as_view(), name='jewelries'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('core/images/favicon.ico'))),
     path('', Home.as_view(), name='home')
 ]
