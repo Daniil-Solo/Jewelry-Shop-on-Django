81c33c760c118892ce4c090da2a6fc67df7e51e3
Daniil-Solo <daniil.solo1723@gmail.com>
2022-04-12 12:11:54 +0500

    add cart_forced_to_update in session

--- jewshop/cart/cart.py
+++ jewshop/cart/cart.py
@@ -1,60 +1,60 @@
-from jewshop.settings import CART_SESSION_ID
+from jewshop.settings import CART_SESSION_ID, CART_FORCED_TO_UPDATE
 from core.models import Jewelry
 
 
 class Cart(object):
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get(CART_SESSION_ID)
-        if not cart:
-            cart = self.session[CART_SESSION_ID] = {}
-        self.cart = cart
-        if 'forced_to_update' not in self.cart:
-            self.cart['forced_to_update'] = False
+        products = self.session.get(CART_SESSION_ID)
+        cart_updated = self.session.get(CART_FORCED_TO_UPDATE)
+        if not products:
+            products = self.session[CART_SESSION_ID] = {}
+        if not cart_updated:
+            cart_updated = self.session[CART_FORCED_TO_UPDATE] = False
+        self.products = products
+        self.products_updated = cart_updated
 
     def is_not_empty(self):
-        jewelry_slugs = set(self.cart.keys())
-        jewelry_slugs.remove('forced_to_update')
-        return len(jewelry_slugs) > 0
+        return len(self.products) > 0
 
     def set_forced_to_update(self, value):
-        self.cart['forced_to_update'] = value
-        self.save()
+        self.products_updated = value
+        self.session[CART_FORCED_TO_UPDATE] = self.products_updated
+        self.session.modified = True
 
     def forced_to_update(self):
-        return self.cart['forced_to_update']
+        return self.products_updated
 
     def add(self, jewelry):
         jewelry_slug = jewelry.slug
-        if jewelry_slug not in self.cart:
-            self.cart[jewelry_slug] = {
+        if jewelry_slug not in self.products:
+            self.products[jewelry_slug] = {
                 'quantity': 0,
                 'price': jewelry.price
             }
-        self.cart[jewelry_slug]['quantity'] += 1
+        self.products[jewelry_slug]['quantity'] += 1
         self.save()
 
     def set_quantity(self, jewelry, new_quantity):
         jewelry_slug = jewelry.slug
-        self.cart[jewelry_slug] = {
+        self.products[jewelry_slug] = {
             'quantity': new_quantity,
             'price': jewelry.price
         }
         self.save()
 
     def save(self):
-        self.session[CART_SESSION_ID] = self.cart
+        self.session[CART_SESSION_ID] = self.products
         self.session.modified = True
 
     def remove(self, jewelry):
         jewelry_slug = jewelry.slug
-        if jewelry_slug in self.cart:
-            del self.cart[jewelry_slug]
+        if jewelry_slug in self.products:
+            del self.products[jewelry_slug]
             self.save()
 
     def __iter__(self):
-        jewelry_slugs = set(self.cart.keys())
-        jewelry_slugs.remove('forced_to_update')
+        jewelry_slugs = self.products.keys()
         jewelry_slugs = list(jewelry_slugs)
         jewelries = (
             Jewelry
@@ -66,19 +66,19 @@ class Cart(object):
             jew_slug = jew.slug
             item = dict(
                 product=jew,
-                price=float(self.cart[jew_slug]['price']),
-                quantity=int(self.cart[jew_slug]['quantity']),
+                price=float(self.products[jew_slug]['price']),
+                quantity=int(self.products[jew_slug]['quantity']),
             )
             item['total_price'] = item['price'] * item['quantity']
             yield item
 
     def get_total_price(self):
-        return sum(item['price'] * item['quantity'] for item in self.cart.values() if type(item) != bool)
+        return sum(item['price'] * item['quantity'] for item in self.products.values())
 
     def get_current_quantity(self, jewelry):
         jewelry_slug = jewelry.slug
-        if jewelry_slug in self.cart:
-            return self.cart[jewelry_slug]["quantity"]
+        if jewelry_slug in self.products:
+            return self.products[jewelry_slug]["quantity"]
         else:
             return 0
 
--- jewshop/jewshop/settings.py
+++ jewshop/jewshop/settings.py
@@ -141,3 +141,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CART_SESSION_ID = 'cart'
+CART_FORCED_TO_UPDATE = 'cart-updated'
