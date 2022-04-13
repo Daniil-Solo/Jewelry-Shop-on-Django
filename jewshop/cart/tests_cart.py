3a86f57e7d418f8ccedf79813972e5cbde8c4aec
Daniil-Solo <daniil.solo1723@gmail.com>
2022-04-13 23:42:41 +0500

    correct, comment cart tests

--- jewshop/cart/tests_cart.py
+++ jewshop/cart/tests_cart.py
@@ -34,32 +34,45 @@ class CartTests(TestCase):
         self.jew2.material_cats.add(self.material)
 
     def test_initial_cart(self):
+        """
+        Проверяет, что для нового пользователя корзина будет пустой
+        """
         self.client.get(reverse('home'))
         current_cart = self.client.session.get("cart")
         self.assertEqual(current_cart, {})
 
     def test_successful_add_jewelry_to_cart(self):
+        """
+        Проверяет, что после добавления товара в корзину, он действительно появляется там
+        """
         response = self.client.post(reverse('cart_add', kwargs=dict(jew_slug=self.jew.slug)))
         current_cart = self.client.session.get("cart")
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(current_cart, {'test_jew_slug': {'price': 100.0, 'quantity': 1}})
+        self.assertEqual(current_cart, {self.jew.slug: {'price': self.jew.price, 'quantity': 1}})
 
     def test_successful_remove_jewelry_from_cart(self):
-        self.client.post(reverse('cart_add', kwargs=dict(jew_slug=self.jew.slug)), {
-            'quantity': 1
-        })
+        """
+        Проверяет удаление товара из корзины
+        """
+        self.client.post(reverse('cart_add', kwargs=dict(jew_slug=self.jew.slug)))
         response = self.client.get(reverse('cart_remove', kwargs=dict(jew_slug=self.jew.slug)))
         current_cart = self.client.session.get("cart")
         self.assertEqual(response.status_code, 302)
         self.assertEqual(current_cart, {})
 
     def test_only_empty_cart(self):
+        """
+        Проверяет страницу с пустой корзины
+        """
         response = self.client.get(reverse('cart'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Корзина")
+        self.assertEqual(response.context_data.get('title'), "Корзина")
         self.assertTemplateUsed(response, "cart/cart.html")
 
     def test_cart_with_one_jewelry(self):
+        """
+        Проверяет страницу корзины с одним товаром и изменением цены на этот товар
+        """
         old_price = self.jew.price
         self.client.post(reverse('cart_add', kwargs=dict(jew_slug=self.jew.slug)))
         new_price = 200.0
@@ -74,9 +87,12 @@ class CartTests(TestCase):
         self.assertEqual(some_jew_in_cart.get("total_price"), 2 * old_price)
 
     def test_cart_with_jewelries(self):
+        """
+        Проверяет страницу корзины с несколькими украшениями
+        """
         self.client.post(reverse('cart_add', kwargs=dict(jew_slug=self.jew.slug)))
         self.client.post(reverse('cart_add', kwargs=dict(jew_slug=self.jew.slug)))
         self.client.post(reverse('cart_add', kwargs=dict(jew_slug=self.jew2.slug)))
         self.client.get(reverse('cart'))
         cart = Cart(RequestRecord(session=self.client.session))
-        self.assertEqual(cart.get_total_price(), 3*self.jew.price)
+        self.assertEqual(cart.get_total_price(), 2*self.jew.price+self.jew2.price)
